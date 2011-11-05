3d8bed03212c8ace58b67f3be8585f710846c7cd
Erik <erik@erikkristensen.com>
2011-11-05 12:29:28 -0400

    several bug fixes

--- salesforce_webform.js
+++ salesforce_webform.js
@@ -4,23 +4,20 @@
 Drupal.behaviors.salesforceWebformFieldsetSummaries = {
   attach: function (context) {
     $('fieldset.salesforce-webform-settings-form', context).drupalSetSummary(function (context) {
-      var use = $("input[@name=use_salesforce]:checked").val();
+      var use = $("input#edit-salesforce-webform-settings-use-salesforce:checked").val();
 
       if (use == 1)
       {
 		$('div.form-item-salesforce-object').removeClass("form-disabled")
-        $("select#edit-salesforce-object").removeAttr("disabled");
+        $("select#edit-salesforce-webform-settings-salesforce-object").removeAttr("disabled");
       }
       else
       {
         $('div.form-item-salesforce-object').addClass("form-disabled")
-        $("select#edit-salesforce-object").attr("disabled", "disabled");
+        $("select#edit-salesforce-webform-settings-salesforce-object").attr("disabled", "disabled");
       }
 
-      return use == true ?
-        Drupal.t('Enabled, Object: @object', { '@object': $("select#edit-salesforce-object option:selected").val() }) :
-        Drupal.t('Disabled');
-
+      return use ? Drupal.t('Enabled, Object: @object', { '@object': $("select#edit-salesforce-webform-settings-salesforce-object").val() }) : Drupal.t('Disabled');
     });
   }
 };
